a61cd7e2222593caf9d6de63f8e4b4f836e2ad97
Mohammed Faran <faranmohammed9223@gmail.com>
2026-04-07 17:11:22 -0400

    Add setup.py for PSDM models package
    Added setup configuration for PSDM models package.

--- HSDM/HSDMIX.py
+++ HSDM/HSDMIX.py
@@ -1,6 +1,46 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from scipy.optimize import minimize
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 24, 2021
+@author: UCChEJBB
+"""
 
-from ixpy.hsdmix import HSDMIX
+"""setup.py: setuptools control."""
+
+
+from setuptools import setup
+import os
+
+cwd = os.getcwd()
+# print(f"ixpy @ {cwd}../../Water_Treatment_Models/IonExchangeModel/ixpy")
+
+with open("README.md", "rb") as f:
+    long_descr = f.read().decode("utf-8")
+
+
+setup(
+    name = "PSDM-models",
+    packages = ["PSDM",],
+
+    version = "0.1.0",
+    description = "Pore and Surface Diffusion Models",
+    long_description = long_descr,
+    author = "Jonathan Burkhardt, Levi Haupert, Boris Datsov",
+    author_email = "burkhardt.jonathan@epa.gov",
+    license='MIT',
+    url='https://github.com/USEPA/Water_Treatment_Models',
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering"],
+    install_requires = [
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "scipy",
+        "openpyxl",
+        "xlsxwriter",
+        f"ixpy @ ..\\..\\Water_Treatment_Models\\IonExchangeModel\\"   
+    ],
+    )
+
